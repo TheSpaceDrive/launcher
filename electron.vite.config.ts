af44ea70b31d5b3b53a64dbad23c0085f7bc123b
StelStelox <andrejt384@gmail.com>
2024-08-02 17:41:05 +0300

    Fix

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,24 +1,49 @@
 import { join } from 'path';
 
-import { defineConfig } from 'electron-vite';
+import { defineConfig, defineViteConfig  } from 'electron-vite';
 
 const toDir = (dir: string) => join(__dirname, dir);
 
 export default defineConfig({
-    main: {
-        plugins: [],
-        build: {
-            sourcemap: true,
-        },
-        resolve: {
-            alias: [
-                {
-                    find: '@config',
-                    replacement: toDir('config.ts'),
+    main: defineViteConfig(({command}) => {
+        if (command === "build") {
+            return {
+                plugins: [],
+                build: {
+                    sourcemap: true,
+                    minify: true,
                 },
-            ],
-        },
-    },
+                resolve: {
+                    alias: [
+                        {
+                            find: '@config',
+                            replacement: toDir('config.ts'),
+                        },
+                    ],
+                },
+            }
+        } else {
+            return {
+                plugins: [],
+                build: {
+                    sourcemap: true,
+                    rollupOptions: {
+                        treeshake: {
+                            annotations: false
+                        }
+                    }
+                },
+                resolve: {
+                    alias: [
+                        {
+                            find: '@config',
+                            replacement: toDir('config.ts'),
+                        }, 
+                    ],
+                },
+            }
+        }
+    }),
     preload: {
         build: {
             sourcemap: true,
@@ -26,6 +51,8 @@ export default defineConfig({
     },
     renderer: {
         build: {
+            minify: true,
+            chunkSizeWarningLimit: 1000,
             sourcemap: true,
         },
     },
